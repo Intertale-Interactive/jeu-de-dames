d98e8ff16974e3d7481e00e2997c3248672f0dee
Intertale Interactve <68112752+Intertale-Interactive@users.noreply.github.com>
2022-12-29 12:52:58 +0100

    New python file for checkers game 🫠 🤸 

--- test.py
+++ test.py
@@ -20,14 +20,23 @@ noirRempli = PhotoImage(file="Dames-Noir-Rempli-Fond-Noir.png")
 blancRempli = PhotoImage(file="Dames-Blanc-Rempli-Fond-Noir.png")
 blanc = PhotoImage(file="Dames-Blanc_Vide.png")
 noir = PhotoImage(file="Dames-Noir_Vide.png")
+demi_Blanc = PhotoImage(file="demi_Blanc.png")
+demi_Noir = PhotoImage(file="demi_Noir.png")
 dico_lignee = {}
 listee = [blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, 
         noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc,
         blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, 
-        noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc]
+        noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc, noirRempli, blanc,
+        blanc, noir, blanc, noir, blanc, noir, blanc, noir, blanc, noir,
+        noir, blanc, noir, blanc, noir, blanc, noir, blanc, noir, blanc,
+        blanc, blancRempli, blanc, blancRempli, blanc, blancRempli, blanc, blancRempli, blanc, blancRempli,
+        blancRempli, blanc, blancRempli, blanc, blancRempli, blanc, blancRempli, blanc, blancRempli, blanc,
+        blanc, blancRempli, blanc, blancRempli, blanc, blancRempli, blanc, blancRempli, blanc, blancRempli,
+        blancRempli, blanc, blancRempli, blanc, blancRempli, blanc, blancRempli, blanc, blancRempli, blanc]
 print(listee)
 
 def affichage():
+    canva.delete('all')
     x, y = 25, 25
     x2, y2 = 25, 25
     x3, y3 = 25, 25
@@ -38,7 +47,7 @@ def affichage():
     x8, y8 = 25, 25
     x9, y9 = 25, 25
     x10, y10 = 25, 25
-    for i in range(40):
+    for i in range(100):
         if i < 10:
             id_pion = canva.create_image(x, y, image=listee[i])
             x += 50
@@ -51,25 +60,98 @@ def affichage():
         elif i >= 30 and i < 40:
             id_pion = canva.create_image(x4, y + 150, image=listee[i])
             x4 += 50
+        elif i >= 40 and i < 50:
+            id_pion = canva.create_image(x5, y + 200, image=listee[i])
+            x5 += 50
+        elif i >= 50 and i < 60:
+            id_pion = canva.create_image(x6, y + 250, image=listee[i])
+            x6 += 50
+        elif i >= 60 and i < 70:
+            id_pion = canva.create_image(x7, y + 300, image=listee[i])
+            x7 += 50
+        elif i >= 70 and i < 80:
+            id_pion = canva.create_image(x8, y + 350, image=listee[i])
+            x8 += 50
+        elif i >= 80 and i < 90:
+            id_pion = canva.create_image(x9, y + 400, image=listee[i])
+            x9 += 50
+        elif i >= 90 and i < 100:
+            id_pion = canva.create_image(x10, y + 450, image=listee[i])
+            x10 += 50
 
 affichage()
+
+white = True
+def clique(event):
+    global white
+    global x, y
+    x, y = event.x, event.y
+    # listee[randrange(0, 39)] = noirRempli
+    affichage()
+    x = x // 50 + 1
+    y = y // 50 + 1
+    numero_case_touched = (10 * (y - 1) + x) - 1
+    canMove = False
+    first_diagonal = listee[numero_case_touched - 11]
+    second_diagonal = listee[numero_case_touched - 9]
+    third_diagonal = listee[numero_case_touched + 9]
+    fourth_diagonal = listee[numero_case_touched + 11]
+
+    if first_diagonal == noir or second_diagonal == noir or third_diagonal == noir or fourth_diagonal == noir:
+        canMove = True
+    else:
+        canMove = False
+    
+    if white == True:
+        # print(x, y, canMove, listee[numero_case_touched], white, black)
+        if listee[numero_case_touched] == blancRempli:
+            global a_supprimer 
+            a_supprimer = numero_case_touched
+        if first_diagonal == noir and listee[numero_case_touched] == blancRempli:
+            listee[numero_case_touched - 11] = demi_Blanc
+        if second_diagonal == noir and listee[numero_case_touched] == blancRempli:
+            listee[numero_case_touched - 9] = demi_Blanc
+        if third_diagonal == noir and listee[numero_case_touched] == blancRempli:
+            listee[numero_case_touched + 9] = demi_Blanc
+        if fourth_diagonal == noir and listee[numero_case_touched] == blancRempli:
+            listee[numero_case_touched + 11] = demi_Blanc
+        if listee[numero_case_touched] == demi_Blanc:
+            listee[numero_case_touched] = blancRempli
+            for i in range(len(listee)):
+                if listee[i] == demi_Blanc:
+                    listee[i] = noir
+            listee[a_supprimer] = noir
+            white = False
+    else:
+        # print(x, y, canMove, listee[numero_case_touched], white, black)
+        if listee[numero_case_touched] == noirRempli:
+            a_supprimer = numero_case_touched
+        if first_diagonal == noir and listee[numero_case_touched] == noirRempli:
+            listee[numero_case_touched - 11] = demi_Noir
+        if second_diagonal == noir and listee[numero_case_touched] == noirRempli:
+            listee[numero_case_touched - 9] = demi_Noir
+        if third_diagonal == noir and listee[numero_case_touched] == noirRempli:
+            listee[numero_case_touched + 9] = demi_Noir
+        if fourth_diagonal == noir and listee[numero_case_touched] == noirRempli:
+            listee[numero_case_touched + 11] = demi_Noir
+        if listee[numero_case_touched] == demi_Noir:
+            listee[numero_case_touched] = noirRempli
+            for i in range(len(listee)):
+                if listee[i] == demi_Noir:
+                    listee[i] = noir
+            listee[a_supprimer] = noir
+            black = False
+            white = True
+    affichage()
+    # canva.create_oval(x-3, y-3, x+3, y+3, fill='red', outline='')
+
+canva.bind("<Button-1>", clique)
 
 # def change_image(event):
 #     global x, y
 #     x, y = event.x, event.y
 #     print(x, y)
 #     canva.create_image(x, y, image=noirRempli)
-
-def clique(event):
-    global x, y
-    x, y = event.x, event.y
-    listee[0] = noirRempli
-    affichage()
-    print(listee)
-    print(x, y)
-    canva.create_oval(x-3, y-3, x+3, y+3, fill='red', outline='')
-
-canva.bind("<Button-1>", clique)
 
 ## END WITH MAINLOOP ##
 root.mainloop()
